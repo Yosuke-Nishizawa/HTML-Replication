4eba68f094174d4aefd723c5d57edcd6dab2f734
yosuke-nishizawa <puyope@gmail.com>
2019-12-17 23:31:29 +0900

    page12 spアコーディオン途中

---
+++ iSara/js/base.js
@@ -0,0 +1,12 @@
+(function(){
+  const setRotateDeg = function(event, deg) {
+    const id = event.currentTarget.id;
+    $(`[data-target='#${id}'].arrow-page12`).css("transform",`translate(0, 0) rotate(${deg}deg)`);
+  }
+  $(".collapse")
+    .on("show.bs.collapse", event => setRotateDeg(event, 225))
+    .on("hide.bs.collapse", event => setRotateDeg(event, 45))
+    .collapse('hide');
+  // page12 最初のアコーディオンを開いておく
+  $(".list-page12>.item:first-of-type .collapse").collapse('show');
+})();
