d471a09d9dba7aa57c824fc3d2a4572230e07b85
yosuke-nishizawa <puyope@gmail.com>
2020-01-26 18:40:20 +0900

    page18 sp途中

--- iSara/js/base.js
+++ iSara/js/base.js
@@ -1,11 +1,14 @@
 (function(){
   const setRotateDeg = function(event, deg) {
     const id = event.currentTarget.id;
-    $(`[data-target='#${id}'].arrow-page12`).css("transform",`translate(0, -50%) rotate(${deg}deg)`);
+    $(`[data-target='#${id}'].arrow-cmn`).css("transform",`translate(0, -50%) rotate(${deg}deg)`);
+    $(`[data-target='#${id}']>.arrow-cmn`).css("transform",`translate(0, -50%) rotate(${deg}deg)`);
   }
   $(".collapse")
     .on("show.bs.collapse", event => setRotateDeg(event, 225))
     .on("hide.bs.collapse", event => setRotateDeg(event, 45));
   // page12 最初のアコーディオンを開いておく
   $(".list-page12>.item:first-of-type .collapse").collapse('show');
+  // page18 最初のアコーディオンを開いておく
+  $(".list-page18>.item:first-of-type .collapse").collapse('show');
 })();
