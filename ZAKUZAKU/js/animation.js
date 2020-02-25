73b5a09cb30e9d6745b32ea15b905a59bdcb69e0
yosuke-nishizawa <puyope@gmail.com>
2020-02-26 08:51:22 +0900

    news sp途中

--- ZAKUZAKU/js/animation.js
+++ ZAKUZAKU/js/animation.js
@@ -1,50 +1,53 @@
-// const paused = false;
-const paused = true;
-// Top 背景
 (() => {
-  const TARGETS = ['.index_top_bg_01', '.index_top_bg_02', '.index_top_bg_03'];
+  // const paused = false;
+  const paused = true;
   // 次の対象を取得
-  const getNextTarget = current => {
-    const currentIndex = TARGETS.indexOf(current);
-    const nextIndex = currentIndex === (TARGETS.length - 1) ? 0 : currentIndex + 1;
-    return TARGETS[nextIndex];
+  const getNextTarget = (current, targets) => {
+    const currentIndex = targets.indexOf(current);
+    const nextIndex = currentIndex === (targets.length - 1) ? 0 : currentIndex + 1;
+    return targets[nextIndex];
   }
   // 前の対象を取得
-  const getPrevTarget = current => {
-    const currentIndex = TARGETS.indexOf(current);
-    const prevIndex = currentIndex === 0 ? (TARGETS.length - 1) : currentIndex - 1;
-    return TARGETS[prevIndex];
+  const getPrevTarget = (current, targets) => {
+    const currentIndex = targets.indexOf(current);
+    const prevIndex = currentIndex === 0 ? (targets.length - 1) : currentIndex - 1;
+    return targets[prevIndex];
   }
-  // 単一の画像に対してのアニメーション生成
-  // 前の画像をフェードアウトして、現在の画像をズームアウトする。
-  // 次の画像の設定も行う
-  const createImgAnime = target => {
-    const prevTarget = getPrevTarget(target);
-    const nextTarget = getNextTarget(target);
-    return gsap.timeline({delay:-0.1})
-          .set(prevTarget, {'z-index':1, display:'block'})
-          .to(prevTarget, {opacity:0, duration:0.5})
-          .set(prevTarget, {'z-index':0, display:'none'})
-          .set(target, {'z-index':1, display:'block'})
-          .set(nextTarget, {'z-index':0, display:'block', opacity:1, scale:1.2})
-          .fromTo(target, {scale:1.2}, {scale:1, duration:2.5, ease:"none", delay:-0.3});
-  }
-  // 各画像のアニメーションを組み合わせる
-  const animateTop = 
-    gsap.timeline({paused:true})
-        .add(createImgAnime(TARGETS[0]))
-        .add(createImgAnime(TARGETS[1]))
-        .add(createImgAnime(TARGETS[2]));
-  if(!paused){
-    // 再生、無限リピート
-    animateTop.play();
-    animateTop.repeat(-1);
-  }
-})();
-// news
-(() =>{
-  const anime =
-    gsap.timeline({paused: true})
-      .to('.index_news_bgs', {left: '-100%', duration:3});
-  anime.play();
+  // Top 背景///////////////////////////////////////////////////////////
+  (() => {
+    const TARGETS = ['.index_top_bg_01', '.index_top_bg_02', '.index_top_bg_03'];
+    // 単一の画像に対してのアニメーション生成
+    // 前の画像をフェードアウトして、現在の画像をズームアウトする。
+    // 次の画像の設定も行う
+    const createImgAnime = target => {
+      const prevTarget = getPrevTarget(target, TARGETS);
+      const nextTarget = getNextTarget(target, TARGETS);
+      return gsap.timeline({delay:-0.1})
+            .set(prevTarget, {'z-index':1, display:'block'})
+            .to(prevTarget, {opacity:0, duration:0.5})
+            .set(prevTarget, {'z-index':0, display:'none'})
+            .set(target, {'z-index':1, display:'block'})
+            .set(nextTarget, {'z-index':0, display:'block', opacity:1, scale:1.2})
+            .fromTo(target, {scale:1.2}, {scale:1, duration:2.5, ease:"none", delay:-0.3});
+    }
+    // 各画像のアニメーションを組み合わせる
+    const animateTop = 
+      gsap.timeline({paused:true})
+          .add(createImgAnime(TARGETS[0]))
+          .add(createImgAnime(TARGETS[1]))
+          .add(createImgAnime(TARGETS[2]));
+    if(!paused){
+      // 再生、無限リピート
+      animateTop.play();
+      animateTop.repeat(-1);
+    }
+  })();
+  // news //////////////////////////////////////////
+  (() =>{
+    const anime =
+      gsap.timeline({paused: true})
+        .to('.index_news_bgs', {left: '-100%', duration:3});
+    anime.play();
+  })();
+
 })();
