452b835024566306547a33f9736863f69199bc84
yosuke-nishizawa <puyope@gmail.com>
2020-03-05 08:47:49 +0900

    news sp途中

--- ZAKUZAKU/ref/gsap3-test.js
+++ ZAKUZAKU/ref/gsap3-test.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(Draggable);
 (() => {
   const slideDelay = 1.5;
   const slideDuration = 0.3;
@@ -37,15 +38,63 @@
   const proxy = document.createElement("div");
   gsap.set(proxy, { x: "+=0"});
   const transform = gsap.getProperty(proxy);
-  const slideAnimation = gsap.to({}, {duration:0.1});
-  const slideWidth = 0;
-  const wrapWidth = 0;
+  let slideAnimation = gsap.to({}, {duration:0.1});
+  let slideWidth = 0;
+  let wrapWidth = 0;
+  resize();
+
+  const draggable = Draggable.create(proxy, {
+    trigger: ".slides-container",
+    onPress: updateDraggable,
+    onDrag: updateProgress,
+    onThrowUpdate: updateProgress,
+    snap: {
+      x: snapX
+    }
+  });
+  window.addEventListener("resize", resize);
+  prevButton.addEventListener("click", function(){
+    animateSlides(1);
+  });
+  nextButton.addEventListener("click", function(){
+    animateSlides(-1);
+  });
   function autoPlay() {
     if (draggable.isPressed || draggable.isDragging || draggable.isThrowing) {
       timer.restart(true);
     } else {
       animateSlides(-1);
     }
+  }
+  function resize() {
+    const norm = (transform.x / wrapWidth) || 0;
+    slideWidth = slides[0].offsetWidth;
+    wrapWidth = slideWidth * numSlides;
+    gsap.set(proxy, {x: norm * wrapWidth});
+    animateSlides(0);
+    slideAnimation.progress(1);
+  }
+  function updateDraggable() {
+    timer.restart(true);
+    slideAnimation.kill();
+    this.update();
+  }
+  function animateSlides(direction) {
+    timer.restart(true);
+    slideAnimation.kill();
+
+    const x = snapX(transform.x + direction * slideWidth);
+    slideAnimation = gsap.to(proxy, {
+      duration: slideDuration,
+      x: x,
+      onUpdate: updateProgress
+    });
+  }
+  function updateProgress() {
+    animation.progress(transform.x / wrapWidth);
+  }
+  function snapX(x) {
+    return Math.round(x / slideWidth) * slideWidth;
   }
 })();
 const TARGETS = ['.img1', '.img2', '.img3'];
