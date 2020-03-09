c3a745ae8dac5ba9973b493abaf5264a7c015cf1
yosuke-nishizawa <puyope@gmail.com>
2020-03-10 08:51:13 +0900

    news sp途中

--- ZAKUZAKU/ref/slide/slide.js
+++ ZAKUZAKU/ref/slide/slide.js
@@ -1,53 +1,98 @@
+// drag pluginの追加
+// https://greensock.com/docs/v3/Plugins/Draggable
 gsap.registerPlugin(Draggable);
-const slideDelay = 1.5;
-const slideDuration = 0.3;
-const slides = document.querySelectorAll("#slide-sample .slide");
+const slideDelay = 1.5;//animation間隔(s)
+const slideDuration = 0.3;//
+//slide群
+const slides = document.querySelectorAll(".slide");
 const prevButton = document.querySelector("#prevButton");
 const nextButton = document.querySelector("#nextButton");
 const numSlides = slides.length;
-gsap.set(slides, {
-  backgroundColor: () => {
-    const num = Math.random() * 0xffffff;
-    return '#' + parseInt(num).toString(16);
-  },
-  xPercent: i => i * 100
-});
-const wrapPartial = (min, max) => {
-  const r = max - min;
-  return value => {
-    const v = value - min;
-    return ((r + v % r) % r) + min;
+//main処理
+(()=>{
+  //初期処理
+  init();
+  //main animation生成
+  const animation = createAnimation();
+  // ???
+  const proxy = document.createElement("div");
+  gsap.set(proxy, { x: "+=0"});
+  const transform = gsap.getProperty(proxy);
+  // animationの更新関数定義
+  function updateProgress() {
+    const v = transform("x") / wrapWidth % 1;
+    const n = v < 0 ? 1 + v : v;
+    animation.progress(n);
   }
-};
-const wrap = wrapPartial(-100, (numSlides - 1) * 100);
-const timer = gsap.delayedCall(slideDelay, autoPlay);
-const animation = gsap.to(slides, {
-  duration: 1,
-  xPercent: "+=" + (numSlides * 100),
-  ease: "none",
-  paused: true,
-  repeat: -1,
-  modifiers: {
-    xPercent: wrap
-  }
-})
-const proxy = document.createElement("div");
-gsap.set(proxy, { x: "+=0"});
-const transform = gsap.getProperty(proxy);
+  const timer = gsap.delayedCall(slideDelay, function(){
+    if (draggable.isPressed || draggable.isDragging || draggable.isThrowing) {
+      timer.restart(true);
+    } else {
+      animateSlides(-1);
+    }
+  });
+  const draggable = createDraggable(proxy, )
+})();
+function init(){
+  //slideにランダムで色付けを行う
+  gsap.set(slides, {
+    backgroundColor: () => {
+      const num = Math.random() * 0xffffff;
+      return '#' + parseInt(num).toString(16);
+    },
+    xPercent: i => i * 100
+  });
+}
+// main animation生成
+function createAnimation(){
+  // slideのx座標決定関数を生成する関数
+  // minとmaxの間になるように値が設定される
+  const wrapPartial = (min, max) => {
+    const r = max - min;
+    return value => {
+      const v = value - min;
+      return v % r + min;
+    }
+  };
+  // slideのx座標決定関数
+  const wrap = wrapPartial(-100, (numSlides - 1) * 100);
+  return gsap.to(slides, {
+    duration: 1,
+    xPercent: "+=" + (numSlides * 100),
+    ease: "none",
+    paused: true,
+    repeat: -1,
+    modifiers: {
+      xPercent: wrap
+    }
+  })
+}
+//ドラッグアニメーションの生成
+function createDraggable(dragTarget, timer, slideAnimation, updateProgress) {
+  const draggable = Draggable.create(dragTarget, {
+    trigger: ".slides-container",
+    //update draggable
+    onPress: function(){
+      timer.restart(true);
+      slideAnimation.kill();
+      this.update();
+    },
+    onDrag: updateProgress,
+    onThrowUpdate: updateProgress,
+    snap: {
+      x: snapX
+    }
+  });
+
+}
+function snapX(x) {
+  return Math.round(x / slideWidth) * slideWidth;
+}
 let slideAnimation = gsap.to({}, {duration:0.1});
 let slideWidth = 0;
 let wrapWidth = 0;
 resize();
 
-const draggable = Draggable.create(proxy, {
-  trigger: ".slides-container",
-  onPress: updateDraggable,
-  onDrag: updateProgress,
-  onThrowUpdate: updateProgress,
-  snap: {
-    x: snapX
-  }
-});
 window.addEventListener("resize", resize);
 prevButton.addEventListener("click", function(){
   animateSlides(1);
@@ -55,13 +100,6 @@ prevButton.addEventListener("click", function(){
 nextButton.addEventListener("click", function(){
   animateSlides(-1);
 });
-function autoPlay() {
-  if (draggable.isPressed || draggable.isDragging || draggable.isThrowing) {
-    timer.restart(true);
-  } else {
-    animateSlides(-1);
-  }
-}
 function resize() {
   const norm = (transform("x") / wrapWidth) || 0;
   slideWidth = slides[0].offsetWidth;
@@ -69,11 +107,6 @@ function resize() {
   gsap.set(proxy, {x: norm * wrapWidth});
   animateSlides(0);
   slideAnimation.progress(1);
-}
-function updateDraggable() {
-  timer.restart(true);
-  slideAnimation.kill();
-  this.update();
 }
 function animateSlides(direction) {
   timer.restart(true);
@@ -85,12 +118,4 @@ function animateSlides(direction) {
     x: x,
     onUpdate: updateProgress
   });
-}
-function updateProgress() {
-  const v = transform("x") / wrapWidth % 1;
-  const n = v < 0 ? 1 + v : v;
-  animation.progress(n);
-}
-function snapX(x) {
-  return Math.round(x / slideWidth) * slideWidth;
 }
