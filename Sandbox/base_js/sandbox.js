f7820aaeae5dcb4be45682c0455d4243a5cc1e93
yosuke-nishizawa <puyope@gmail.com>
2020-04-06 23:20:05 +0900

    news pc途中

--- Sandbox/base_js/sandbox.js
+++ Sandbox/base_js/sandbox.js
@@ -7,12 +7,28 @@ function setWidth() {
   widthLabel.textContent = window.innerWidth;
 }
 
-const labelTagets = document.getElementsByClassName('test');
+const labelTagets = document.querySelectorAll('body div');
+// const labelTagets = document.getElementsByClassName('test');
 for(const e of labelTagets){
   const label = document.createElement('span');
+  label.className = 'label';
   const style = window.getComputedStyle(e);
-  const text = `${e.className}
-${style['min-width']}`;
-  label.textContent = text;
+  const event = function() {
+    const text = `${e.classList[0]}\n${style['min-width']}~${style['max-width']}\n${style['width']}`;
+    label.textContent = text;
+  }
+  event();
+  window.addEventListener('resize', event);
   e.insertBefore(label, e.firstChild);
+  // const tooltip = document.createElement('span');
+  // tooltip.className = 'tooltip';
+  // tooltip.textContent = e.className;
+  // tooltip.style.display = 'hidden';
+  // label.appendChild(tooltip);
+  // label.addEventListener('mouseenter', function() {
+  //   tooltip.style.display = 'block';
+  // });
+  // label.addEventListener('mouseleave', function() {
+  //   tooltip.style.display = 'hidden';
+  // });
 }
