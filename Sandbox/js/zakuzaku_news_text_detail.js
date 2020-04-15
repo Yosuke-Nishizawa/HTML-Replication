07e737ae0a9f35ff0122d71faa3859484727a3c7
yosuke-nishizawa <puyope@gmail.com>
2020-04-16 08:51:30 +0900

    news pc途中

--- Sandbox/js/zakuzaku_news_text_detail.js
+++ Sandbox/js/zakuzaku_news_text_detail.js
@@ -1,2 +1,20 @@
-const anime = gsap.timeline()
-                  .to(".detail", {'bottom': -10, duration+})
+const anime = gsap.timeline({paused: true})
+                  .to(".detail", {
+                    'bottom': -30,
+                    'opacity': 0,
+                    //  duration: 0.2,
+                    //  ease: "none",
+                      stagger: {
+                        each: 0.1,
+                        from: "start"
+                      }
+
+                    });
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", function() {
+  anime.play();
+})
+const reverseButton = document.getElementById("reverse");
+reverseButton.addEventListener("click", function() {
+  anime.reverse();
+})
